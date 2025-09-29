2443e698ff3c57f8e4ac5441eddf679297fc64fb
chaman khandelwal <chamankhandelwal5@gmail.com>
2025-09-29 14:47:52 +0530

    IsAuth

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -3,12 +3,9 @@ import { User } from "../models/User.js";
 
 export const isAuth = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    const token = req.headers.token;
+    if (!token)
       return res.status(401).json({ message: "No token provided" });
-    }
-
-    const token = authHeader.split(" ")[1];
 
     const decodedData = jwt.verify(token, process.env.JWT_SEC);
     req.user = await User.findById(decodedData._id);
