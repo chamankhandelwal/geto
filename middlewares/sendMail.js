329f75777bc4a910e6f4bc9d56db3de216bd6b69
chaman khandelwal <chamankhandelwal5@gmail.com>
2025-09-29 21:46:54 +0530

    Outlook

--- middlewares/sendMail.js
+++ middlewares/sendMail.js
@@ -1,17 +1,31 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 const sendMail = async (email, subject, data) => {
   try {
-    await resend.emails.send({
-      from: 'GetO <onboarding@resend.dev>',
+    const transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.OUTLOOK_EMAIL,
+        pass: process.env.OUTLOOK_APP_PASSWORD,
+      },
+    });
+
+    const html = `
+      <h1>Hello ${data.name}</h1>
+      <p>Your OTP is: <strong>${data.otp}</strong></p>
+      <p>This OTP will expire in 3 minutes</p>
+    `;
+
+    await transporter.sendMail({
+      from: `GetO <${process.env.OUTLOOK_EMAIL}>`,
       to: email,
       subject,
-      html: `<h1>Hello ${data.name}</h1>
-             <p>Your OTP is: <strong>${data.otp}</strong></p>
-             <p>This OTP will expire in 3 minutes</p>`
+      html,
     });
+
+    console.log("Email sent successfully to", email);
   } catch (err) {
     console.error("Error sending email:", err);
     throw err;
