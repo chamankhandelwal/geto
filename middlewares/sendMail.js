4c1e666ee7ccacb40965da6d2c17fed6d1189fa8
chaman khandelwal <chamankhandelwal5@gmail.com>
2025-09-29 22:08:58 +0530

    Brevo

--- middlewares/sendMail.js
+++ middlewares/sendMail.js
@@ -3,23 +3,23 @@ import nodemailer from "nodemailer";
 const sendMail = async (email, subject, data) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.office365.com",
+      host: "smtp-relay.brevo.com",
       port: 587,
-      secure: false,
+      secure: false, // use TLS
       auth: {
-        user: process.env.OUTLOOK_EMAIL,
-        pass: process.env.OUTLOOK_APP_PASSWORD,
+        user: "9821db001@smtp-brevo.com", // your SMTP login
+        pass: "d60pRQJtBj4g3HPM",        // your SMTP key / master password
       },
     });
 
     const html = `
       <h1>Hello ${data.name}</h1>
       <p>Your OTP is: <strong>${data.otp}</strong></p>
-      <p>This OTP will expire in 3 minutes</p>
+      <p>This OTP will expire in 3 minutes. Do not share it with anyone.</p>
     `;
 
     await transporter.sendMail({
-      from: `GetO <${process.env.OUTLOOK_EMAIL}>`,
+      from: `GetO <9821db001@smtp-brevo.com>`, // same as SMTP login
       to: email,
       subject,
       html,
